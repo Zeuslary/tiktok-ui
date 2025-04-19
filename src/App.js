489f5db0bf468b5aabb1804332b8d756db244d9c
Zeus <daothingocchiem@gmail.com>
2025-04-19 17:53:23 +0700

    Push project to local

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import HeadingPage from "../../react_webpack/src/components/Heading";
+import ParagraphPage from "../../react_webpack/src/components/Paragraph";
+import GlobalStyles from "../../react_webpack/src/components/GlobalStyles";
+import Button from "../../react_webpack/src/components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalStyles>
+      <div style={{ padding: 10 }}>
+        <h1>Tiktok</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/heading">Heading</Link>
+            </li>
+            <li>
+              <Link to="/paragraph">Paragraph</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h3>Home page</h3>
+              <p>This is the home page of the app.</p>
+            </div>
+          }
+        />
+        <Route path="/heading" element={<HeadingPage />} />
+        <Route path="/paragraph" element={<ParagraphPage />} />
+      </Routes>
+    </GlobalStyles>
   );
 }
 
